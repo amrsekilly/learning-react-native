fd03f69faab49a9cfd9518d7a163e2079e365798
Amr Elsekilly <amr.e.dev@gmail.com>
2017-06-19 04:10:43 +0200

    learned about images

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+    let img = { uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' };
     return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-      </View>
+      <Image source={img} style={{width: 193, height: 110}}/>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'cyan',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
